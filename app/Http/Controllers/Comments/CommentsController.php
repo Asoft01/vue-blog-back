f05fa7a64a5092fa35e92ed8f684370fc0ba9ad6
Adeleke Hammed <lekhad19@gmail.com>
2021-08-12 00:25:17 +0100

    CommentsController

---
+++ app/Http/Controllers/Comments/CommentsController.php
@@ -0,0 +1,56 @@
+<?php
+
+namespace App\Http\Controllers\Comments;
+
+use App\Comment;
+use App\Http\Controllers\Controller;
+use Illuminate\Http\Request;
+
+class CommentsController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function index()
+    {
+        $comments = Comment::all();
+        return ['comments' => $comments];
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @return \Illuminate\Http\Response
+     */
+    public function store(Request $request)
+    {
+        //
+    }
+
+
+    /**
+     * Update the specified resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function update(Request $request, $id)
+    {
+        //
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     *
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function destroy($id)
+    {
+        //
+    }
+}
